1deb7779738ece28156df1ee5541d85a8a554a96
Anishka Ratnawat <67223010+Firstpage12@users.noreply.github.com>
2023-02-10 00:28:27 +0530

    Update urls.py

--- djangoStudent/lem/urls.py
+++ djangoStudent/lem/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    url(r'^', include('employee.urls')),
+    url('', include('employee.urls')),
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    
 ]
